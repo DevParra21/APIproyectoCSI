eead8038341f4c19c81d74a9f569f6c35e3973be
PEEP VS Desktop <patricio.escuderoprr@uanl.edu.mx>
2020-06-06 08:29:33 -0500

    initial Commit

---
+++ eclipse-workspace/ProyectoCSI_API_REST/src/main/java/com/proyectocsi/api/rest/ProyectoCsiApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.proyectocsi.api.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCsiApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoCsiApiRestApplication.class, args);
+	}
+
+}
